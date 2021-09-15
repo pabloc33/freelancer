bf55740e0e0415cb24b47c85d1f6976ba00e01fb
pabloc33 <pablo.contino33@gmail.com>
2021-09-15 16:02:51 -0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", () => {
+  cargarPortafolio();
+});
+
+function cargarPortafolio() {
+  fetch("datos.json")
+    .then((respuesta) => {
+      return respuesta.json();
+    })
+    .then((datos) => {
+      let html = "";
+
+      datos.portafolio.forEach((portafolio) => {
+        // Crear el template
+
+        html += `
+        <div class="elemento">
+              <img src="img/${portafolio.id}.jpg" >
+              <div class="contenido">
+                 <h3>${portafolio.nombre}</h3>
+                 <p>${portafolio.desc}</p>
+              </div>
+        </div>
+        `;
+      });
+      // Después de crear el template, inyectar al HTML
+      document.querySelector("#listado").innerHTML = html;
+    });
+}
